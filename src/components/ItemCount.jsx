fb0cda3315e2896ef605a52feb7a33ffdc62bf4b
ltherisod <lauratherisod@gmail.com>
2026-02-05 18:46:59 -0300

    styles

--- src/components/ItemCount.jsx
+++ src/components/ItemCount.jsx
@@ -2,7 +2,7 @@ import { useEffect, useState } from "react"
 
 const ItemCount = ({stock})=> {
 const [count, setCount]= useState(1)
-const [comprar, setComprar]= useState(false)
+
 console.log('ItemCount')
 
 const sumar = ()=> {
@@ -18,52 +18,27 @@ const restar= ()=> {
     }
 }
 const purchase = ()=> {
-    setComprar(!comprar)
+    
 }
 
-// //SE EJECUTA SIEMPRE
-// useEffect(()=>{
-//     //CODIGO
-//     //CASI NUNCA / NUNCA
-//     console.log('Me ejecuto SIEMPRE')
-// })
 
-
-// //SE EJECUTE UNA SOLA VEZ
-
-// useEffect(()=>{
-//     //CODIGO
-//     //CASI SIEMPRE
-//     console.log('ME EJECUTO UNA SOLA VEZ, CUANDO SE MONTA EL COMPONENTE')
-// },[])
-
-
-// //SE EJETUTE UNA SOLA VEZ Y QUE ESTE A LA ESCUCHA DE UN CAMBIO
-
-// useEffect(()=>{
-//     //CODIGO
-//     //CASI SIEMPRE
-//     console.log('ME EJECUTO UNA SOLA VEZ, CUANDO SE MONTA EL COMPONENTE Y SIEMPRE QUE COMPRAR CAMBIE', comprar)
-// },[comprar])
-
-
-// useEffect(()=>{
-//     //CODIGO
-//     //CASI NUNCA
-//     console.log('con eventListener')
-//     return()=>{
-//         console.log('efecto de limpieza')
-//     }
-// },[])
 
 
     return(
-        <div>
-            <button className="btn btn-danger" onClick={restar}>-</button>
-            <span className="btn">{count}</span>
-            <button className="btn btn-success" onClick={sumar}>+</button>
-            <button className="btn btn-primary" onClick={purchase}>Comprar </button>
-        </div>
+      <div className="counter-container">
+  <div className="counter-controls">
+    <button className="btn btn-danger" onClick={restar}>-</button>
+
+    <span className="counter-value">{count}</span>
+
+    <button className="btn btn-success" onClick={sumar}>+</button>
+  </div>
+
+  <button className="btn btn-primary buy-btn" onClick={purchase}>
+    Comprar
+  </button>
+</div>
+
     )
 }
 export default ItemCount
